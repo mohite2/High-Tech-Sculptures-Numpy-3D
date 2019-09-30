a2ba9fe4445347581887b704956d5a5bef83843a
Yuerong <yuerong2@gmail.com>
2019-09-30 01:02:08 -0500

    4

--- numpy_marble_solution.py
+++ numpy_marble_solution.py
@@ -10,6 +10,71 @@ import numpy as np
 from scipy.ndimage import center_of_mass
 from typing import List
 
+def are_rotations_unique(list_of_rotations: List[List[dict]], verbose=False) -> bool:
+    """Given a list of list of 3D rotation combinations suitable for using with np.rot90()
+    and as returned from the get_orientations_possible() function, determine whether any
+    of the rotations are equivalent, and discard the duplicates.
+
+    The purpose is to detect situations where a combination of rotations would produce either
+    the original unmodified array or the same orientation as any previous one in the list.
+
+    NOTE: This function is already complete! It is provided as an example of rotation
+    calculations, good Doctests, and it could be useful to you as part of your solution.
+
+    :param list_of_rotations: a list, such as returned by get_orientations_possible()
+    :param verbose: if True, will print details to console, otherwise silent.
+    :return: True, if all listed rotation combinations produce distinct orientations.
+
+    >>> x = [[{'k': 4, 'axes': (0, 1)}]]  # 4x90 degrees is a full rotation
+    >>> are_rotations_unique(x)
+    False
+    >>> x = [[{'k': 2, 'axes': (0, 1)}, {'k': 2, 'axes': (0, 1)}]]  # also a full rotation
+    >>> are_rotations_unique(x)
+    False
+    >>> y1 = [[{'k': 3, 'axes': (1, 2)}], [{'k': 1, 'axes': (0, 1)}, {'k': 1, 'axes': (2, 0)}]]
+    >>> are_rotations_unique(y1)
+    True
+    >>> y2 = y1 + [[{'k': 1, 'axes': (1, 2)}, {'k': 3, 'axes': (1, 0)}]]  # equiv. to earlier
+    >>> are_rotations_unique(y2, verbose=True)
+    combination #1: [{'k': 3, 'axes': (1, 2)}] ok.
+    combination #2: [{'k': 1, 'axes': (0, 1)}, {'k': 1, 'axes': (2, 0)}] ok.
+    combination #3: [{'k': 1, 'axes': (1, 2)}, {'k': 3, 'axes': (1, 0)}] not unique.
+    it results in the same array as combination 2
+    False
+    """
+    # create a small cube to try all the input rotations. It has unique values so that
+    #  no distinct rotations could create an equivalent array by accident.
+    cube = np.arange(0, 27).reshape((3, 3, 3))
+
+    # Note: In the code below, the arrays must be appended to the orientations_seen
+    #  list in string form, because Numpy would otherwise misunderstand the intention
+    #  of the if ... in orientations_seen expression.
+
+    orientations_seen = [cube.tostring()]  # record the original
+
+    count = 0
+    for combo in list_of_rotations:
+        count += 1
+        if verbose:#why
+            print('combination #{}: {}'.format(count, combo), end='')
+
+        r = cube  # start with a view of cube unmodified for comparison
+        for r90 in combo:  # apply all the rotations given in this combination
+            r = np.rot90(r, k=r90['k'], axes=r90['axes'])
+        if r.tostring() in orientations_seen:
+            if verbose:
+                print(' not unique.')
+                if r.tostring() == cube.tostring():
+                    print('it results in the original 3d array.')
+                else:
+                    print('it results in the same array as combination',
+                          orientations_seen.index(r.tostring()))
+            return False
+        else:
+            if verbose:
+                print(' ok.')
+        orientations_seen.append(r.tostring())
+    return True ###Why
 
 def get_orientations_possible(block: np.ndarray) -> List[List[dict]]:
     """Given a 3D numpy array, look at its shape to determine how many ways it
@@ -81,10 +146,39 @@ def get_orientations_possible(block: np.ndarray) -> List[List[dict]]:
     (height, depth, width) = block.shape
 
     if height == depth == width:
+        print('all in poss')
         return poss  # return all possibilities, it's a cube
+    else:
+        pass
+
+    for value in poss:
+        print([value])
+        #list_of_dict = [{} for i in range(singleRotation)]
+        print(are_rotations_unique([value]))
 
     # TODO: Complete this function for the other situations...
     # Hint, the results will be parts of the 23-item list above, read the Docstring!
+    # elif height==depth and depth !=width:
+    #     # possible=len(are_rotations_unique(poss))
+    #     # print(possible
+    #     print(are_rotations_unique(poss),"1")
+    # elif height!= depth and height!=width and width !=depth:
+    #     # possible = len(are_rotations_unique(poss))
+    #     # print(possible)
+    #     print(are_rotations_unique(poss),"2")
+    # elif height==depth and depth!=width:
+    #     # possible = len(are_rotations_unique(poss))
+    #     # print(possible)
+    #     print(are_rotations_unique(poss),'3')
+    # elif height !=depth and depth==width:
+    #     # possible = len(are_rotations_unique(poss))
+    #     # print(possible)
+    #     print(are_rotations_unique(poss),"4")
+    # elif height==width and depth!=width:
+    #     # possible = len(are_rotations_unique(poss))
+    #     # print(possible)
+    #     print(are_rotations_unique(poss),"5")
+
 
 
 def carve_sculpture_from_density_block(shape: np.ndarray, block: np.ndarray) -> np.ndarray:
@@ -127,71 +221,7 @@ def analyze_sculptures(block_filenames: list, shape_filenames: list):
     # TODO: Add a few good, working Doctests
 
 
-def are_rotations_unique(list_of_rotations: List[List[dict]], verbose=False) -> bool:
-    """Given a list of list of 3D rotation combinations suitable for using with np.rot90()
-    and as returned from the get_orientations_possible() function, determine whether any
-    of the rotations are equivalent, and discard the duplicates.
 
-    The purpose is to detect situations where a combination of rotations would produce either
-    the original unmodified array or the same orientation as any previous one in the list.
-
-    NOTE: This function is already complete! It is provided as an example of rotation
-    calculations, good Doctests, and it could be useful to you as part of your solution.
-
-    :param list_of_rotations: a list, such as returned by get_orientations_possible()
-    :param verbose: if True, will print details to console, otherwise silent.
-    :return: True, if all listed rotation combinations produce distinct orientations.
-
-    >>> x = [[{'k': 4, 'axes': (0, 1)}]]  # 4x90 degrees is a full rotation
-    >>> are_rotations_unique(x)
-    False
-    >>> x = [[{'k': 2, 'axes': (0, 1)}, {'k': 2, 'axes': (0, 1)}]]  # also a full rotation
-    >>> are_rotations_unique(x)
-    False
-    >>> y1 = [[{'k': 3, 'axes': (1, 2)}], [{'k': 1, 'axes': (0, 1)}, {'k': 1, 'axes': (2, 0)}]]
-    >>> are_rotations_unique(y1)
-    True
-    >>> y2 = y1 + [[{'k': 1, 'axes': (1, 2)}, {'k': 3, 'axes': (1, 0)}]]  # equiv. to earlier
-    >>> are_rotations_unique(y2, verbose=True)
-    combination #1: [{'k': 3, 'axes': (1, 2)}] ok.
-    combination #2: [{'k': 1, 'axes': (0, 1)}, {'k': 1, 'axes': (2, 0)}] ok.
-    combination #3: [{'k': 1, 'axes': (1, 2)}, {'k': 3, 'axes': (1, 0)}] not unique.
-    it results in the same array as combination 2
-    False
-    """
-    # create a small cube to try all the input rotations. It has unique values so that
-    #  no distinct rotations could create an equivalent array by accident.
-    cube = np.arange(0, 27).reshape((3, 3, 3))
-
-    # Note: In the code below, the arrays must be appended to the orientations_seen
-    #  list in string form, because Numpy would otherwise misunderstand the intention
-    #  of the if ... in orientations_seen expression.
-
-    orientations_seen = [cube.tostring()]  # record the original
-
-    count = 0
-    for combo in list_of_rotations:
-        count += 1
-        if verbose:
-            print('combination #{}: {}'.format(count, combo), end='')
-
-        r = cube  # start with a view of cube unmodified for comparison
-        for r90 in combo:  # apply all the rotations given in this combination
-            r = np.rot90(r, k=r90['k'], axes=r90['axes'])
-        if r.tostring() in orientations_seen:
-            if verbose:
-                print(' not unique.')
-                if r.tostring() == cube.tostring():
-                    print('it results in the original 3d array.')
-                else:
-                    print('it results in the same array as combination',
-                          orientations_seen.index(r.tostring()))
-            return False
-        else:
-            if verbose:
-                print(' ok.')
-        orientations_seen.append(r.tostring())
-    return True
 
 
 if __name__ == '__main__':
@@ -199,7 +229,7 @@ if __name__ == '__main__':
     #  just some examples of loading and manipulating the arrays.
 
     # Load a "block" of variable-density marble:
-    marble_block_1 = np.load(file='data/marble_block_1.npy')
+    marble_block_1 = np.load(file='data/marble_block_5.npy')
 
     # Load one array describing the 3D shape of the sculpture we want to carve from marble:
     shape_1 = np.load(file='data/shape_1.npy')
@@ -211,8 +241,11 @@ if __name__ == '__main__':
     # marble_block_1[shape_1 == 0] = 0
     # print(marble_block_1.shape)
     # print(marble_block_1)
-    list1 = [[{'k': 1, 'axes': (1, 2)}, {'k': 2, 'axes': (0, 2)}]]
-    list2 = [[{'k': 1, 'axes': (1, 2)}, {'k': 3, 'axes': (0, 2)}]]
-    are_rotations_unique(list1)
-    are_rotations_unique(list2)
+    # list1 = [[{'k': 1, 'axes': (1, 2)}, {'k': 2, 'axes': (0, 2)}]]
+    # list2 = [[{'k': 1, 'axes': (1, 2)}, {'k': 3, 'axes': (0, 2)}]]
+    # are_rotations_unique(list1)
+    # are_rotations_unique(list2)
+    marble_block_7 = np.arange(36).reshape((3,3,4))
+    #print(marble_block_7)
+    get_orientations_possible(marble_block_1)
     # print('mean density of unmodified block: {:.2f}'.format(np.nanmean(marble_block_1.astype('float32'))))
---
+++ transpose testing.py
@@ -0,0 +1,74 @@
+import numpy as np
+import importlib
+from scipy.ndimage import center_of_mass
+from typing import List
+
+def are_rotations_unique(list_of_rotations: List[List[dict]], verbose=True) -> bool:
+    """Given a list of list of 3D rotation combinations suitable for using with np.rot90()
+    and as returned from the get_orientations_possible() function, determine whether any
+    of the rotations are equivalent, and discard the duplicates.
+
+    The purpose is to detect situations where a combination of rotations would produce either
+    the original unmodified array or the same orientation as any previous one in the list.
+
+    NOTE: This function is already complete! It is provided as an example of rotation
+    calculations, good Doctests, and it could be useful to you as part of your solution.
+
+    :param list_of_rotations: a list, such as returned by get_orientations_possible()
+    :param verbose: if True, will print details to console, otherwise silent.
+    :return: True, if all listed rotation combinations produce distinct orientations.
+
+    >>> x = [[{'k': 4, 'axes': (0, 1)}]]  # 4x90 degrees is a full rotation
+    >>> are_rotations_unique(x)
+    False
+    >>> x = [[{'k': 2, 'axes': (0, 1)}, {'k': 2, 'axes': (0, 1)}]]  # also a full rotation
+    >>> are_rotations_unique(x)
+    False
+    >>> y1 = [[{'k': 3, 'axes': (1, 2)}], [{'k': 1, 'axes': (0, 1)}, {'k': 1, 'axes': (2, 0)}]]
+    >>> are_rotations_unique(y1)
+    True
+    >>> y2 = y1 + [[{'k': 1, 'axes': (1, 2)}, {'k': 3, 'axes': (1, 0)}]]  # equiv. to earlier
+    >>> are_rotations_unique(y2, verbose=True)
+    combination #1: [{'k': 3, 'axes': (1, 2)}] ok.
+    combination #2: [{'k': 1, 'axes': (0, 1)}, {'k': 1, 'axes': (2, 0)}] ok.
+    combination #3: [{'k': 1, 'axes': (1, 2)}, {'k': 3, 'axes': (1, 0)}] not unique.
+    it results in the same array as combination 2
+    False
+    """
+    # create a small cube to try all the input rotations. It has unique values so that
+    #  no distinct rotations could create an equivalent array by accident.
+    cube = np.arange(0, 54).reshape((6, 3, 3))
+
+    # Note: In the code below, the arrays must be appended to the orientations_seen
+    #  list in string form, because Numpy would otherwise misunderstand the intention
+    #  of the if ... in orientations_seen expression.
+
+    orientations_seen = [cube.tostring()]  # record the original
+
+    count = 0
+    for combo in list_of_rotations:
+        count += 1
+        if verbose:
+            print('combination #{}: {}'.format(count, combo), end='')
+
+        r = cube  # start with a view of cube unmodified for comparison
+        for r90 in combo:  # apply all the rotations given in this combination
+            r = np.rot90(r, k=r90['k'], axes=r90['axes'])
+        if r.tostring() in orientations_seen:
+            if verbose:
+                print(' not unique.')
+                if r.tostring() == cube.tostring():
+                    print('it results in the original 3d array.')
+                else:
+                    print('it results in the same array as combination',
+                          orientations_seen.index(r.tostring()))
+            return False
+        else:
+            if verbose:
+                print(' ok.')
+        orientations_seen.append(r.tostring())
+    return True
+
+x=[{'k': 2, 'axes': (0, 1)}, {'k': 3, 'axes': (0, 2)}]
+result=are_rotations_unique([x])
+print(result)
